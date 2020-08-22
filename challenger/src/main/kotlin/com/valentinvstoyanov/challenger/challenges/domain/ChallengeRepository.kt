e5009fd05b025a367a2de734659159cfa2783f01
valentinvstoyanov <valentinstoqnov98@gmail.com>
2020-08-22 07:03:39 +0300

    Add ChallengeRepository

---
+++ challenger/src/main/kotlin/com/valentinvstoyanov/challenger/challenges/domain/ChallengeRepository.kt
@@ -0,0 +1,9 @@
+package com.valentinvstoyanov.challenger.challenges.domain
+
+import com.valentinvstoyanov.challenger.challenges.domain.model.Challenge
+import com.valentinvstoyanov.challenger.challenges.domain.model.CreateChallenge
+import reactor.core.publisher.Mono
+
+interface ChallengeRepository {
+    fun create(challenge: CreateChallenge): Mono<Challenge>
+}
